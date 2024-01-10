2582c6eb071e606bad1f52339ea1ee87bfe48c0c
russgmoore <russgmoore@users.noreply.github.com>
2024-01-10 14:27:29 -0500

    adding certificate example

---
+++ certificate.tf
@@ -0,0 +1,29 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "example" {
+  name                = "examplekeyvault"
+  location                 = var.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "premium"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    certificate_permissions = [
+      "Create",
+      "Delete",
+      "DeleteIssuers",
+      "Get",
+      "GetIssuers",
+      "Import",
+      "List",
+      "ListIssuers",
+      "ManageContacts",
+      "ManageIssuers",
+      "SetIssuers",
+      "Update",
+    ]
+  }
+}
