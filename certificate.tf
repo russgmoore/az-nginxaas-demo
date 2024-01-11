af5db515e4cc5e9f2e13f9ed6953b6535e7653f9
russgmoore <russgmoore@users.noreply.github.com>
2024-01-10 19:26:41 -0500

    update keystore name

--- certificate.tf
+++ certificate.tf
@@ -1,9 +1,9 @@
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault" "example" {
-  name                = "examplekeyvault"
-  location                 = var.location
-  resource_group_name      = azurerm_resource_group.rg.name
+resource "azurerm_key_vault" "kv" {
+  name                   = "kv-${random_pet.rg_name.id}"
+  location               = var.location
+  resource_group_name    = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "premium"
 
@@ -26,4 +26,5 @@ resource "azurerm_key_vault" "example" {
       "Update",
     ]
   }
+  tags = var.tags
 }
